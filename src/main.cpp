1e39e6edb9e927f83809194e500457852376b242
Diogo Krüger <diogoks2005@gmail.com>
2026-01-16 03:53:26 -0300

    pequenas mudanças

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,23 @@
 #include <unistd.h>     // getpid(), gettid(), sleep()
 
 void* printOi(void* arg){
-    std::cout << "oi! o meu TID é " << gettid() << std::endl;
+    std::cout << "oi! o meu Thread ID no computador inteiro é: " << gettid() << std::endl;
+    std::cout << "ah! e meu \"Thread ID\" somente nesse programa é: " << (long) arg << std::endl << std::endl;
     sleep(3);
-    return NULL;
+    pthread_exit(NULL);
 }
 
 int main(void){
 
     pthread_t thread1;
-    pthread_create(&thread1, NULL, &printOi, NULL);
+    if (pthread_create(&thread1, NULL, &printOi, (void *) 1) != 0){
+        return 1;
+    }
 
     pthread_t thread2;
-    pthread_create(&thread2, NULL, &printOi, NULL);
+    if (pthread_create(&thread2, NULL, &printOi, (void *) 2) != 0){
+        return 2;
+    }
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
